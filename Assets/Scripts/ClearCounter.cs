3895bcb64078d17f14bb20c58fd95b620f37d858
hanng67 <giahanng67@gmail.com>
2023-05-10 09:44:55 +0700

    + drag & drop KitchenObject to ClearCounter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -8,5 +8,33 @@ public class ClearCounter : BaseCounter, IKitchenObjectParent
 
     public override void Interact(Player player)
     {
+        if (!HasKitchenObject())
+        {
+            // There is no KitchenObject here
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying something
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                // Player is not carrying anything
+                // Do nothing
+            }
+        }
+        else
+        {
+            // There is a KitchenObject here
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying something
+                // Do nothing
+            }
+            else
+            {
+                // Player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }

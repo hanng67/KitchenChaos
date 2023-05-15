17feff9b39f8e298309b2c67c91270e0e71b2ee9
hanng67 <giahanng67@gmail.com>
2023-05-15 19:00:20 +0700

    - close game pause UI when open options UI

--- Assets/Scripts/UIs/OptionsUI.cs
+++ Assets/Scripts/UIs/OptionsUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -24,6 +25,8 @@ public class OptionsUI : MonoBehaviour
 
     [SerializeField] private Transform pressToRebindKeyTransform;
 
+    private Action onCloseButtonAction;
+
     private void Awake()
     {
         Instance = this;
@@ -41,6 +44,7 @@ public class OptionsUI : MonoBehaviour
         closeButton.onClick.AddListener(() =>
         {
             Hide();
+            onCloseButtonAction();
         });
 
         moveUpKeyBindingUI.AddListenerRebindBinding(() => { RebindBinding(GameInput.Binding.Move_Up); });
@@ -81,8 +85,10 @@ public class OptionsUI : MonoBehaviour
         pauseKeyBindingUI.SetBindingText(GameInput.Instance.GetBindingText(GameInput.Binding.Pause));
     }
 
-    public void Show()
+    public void Show(Action onCloseButtonAction)
     {
+        this.onCloseButtonAction = onCloseButtonAction;
+
         gameObject.SetActive(true);
     }
 

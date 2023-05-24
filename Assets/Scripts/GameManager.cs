1c808425008155233d3604bf0b8c4c0cc84905b2
hanng67 <giahanng67@gmail.com>
2023-05-24 11:07:01 +0700

    ! Bump v0.0.1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -22,7 +22,7 @@ public class GameManager : MonoBehaviour
     private State state;
     private float countdownToStartTimer = 3f;
     private float gamePlayingTimer = 0;
-    private float gamePlayingTimerMax = 60f;
+    private float gamePlayingTimerMax = 60f * 3f;
     private bool isGamePaused = false;
 
     private void Awake()

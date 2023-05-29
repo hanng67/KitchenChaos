d01d509c889c73c7c5f11587dfc3aecbc0725ee0
hanng67 <giahanng67@gmail.com>
2023-05-29 22:17:40 +0700

    + sync player movement with client auth

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -57,6 +57,8 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
 
     private void Update()
     {
+        if (!IsOwner) return;
+
         HandleMovement();
         HandleInteractions();
     }
